f6ab3e8217c7e4d4439e86c0596f22008fb7edc5
AlexeyTopchiev <wearelols001@gmail.com>
2021-11-08 00:03:56 +0300

    Frontend: add dynamic render partners on main page and dynamic menu page

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,55 @@
-const partner = "tanuki"
+const $cardsMenu = document.querySelector(".cards-menu")
+const $sectionHeading = document.querySelector(".section-heading")
 
-const renderItems = data => {
-  data.forEach(el => console.log(el))
+const handleTitle = ({ name, stars, price, kitchen }) => {
+  $sectionHeading.innerHTML = `
+            <h2 class="section-title restaurant-title">${name}</h2>
+            <div class="card-info">
+              <div class="rating">
+                ${stars}
+              </div>
+              <div class="price">От ${price} ₽</div>
+              <div class="category">${kitchen}</div>
+            </div>`
 }
 
-fetch(`./db/${partner}.json`)
-  .then(response => response.json())
-  .then(data => renderItems(data))
-  .catch(error => console.log("###e:", error))
+const renderItems = data => {
+  data.forEach(({ description, id, image, name, price }) => {
+    const $card = document.createElement("div")
+
+    $card.classList.add("card")
+
+    $card.innerHTML = `<img src=${image} alt=${id} class="card-image" />
+						<div class="card-text">
+							<div class="card-heading">
+								<h3 class="card-title card-title-reg">${name}</h3>
+							</div>
+							<div class="card-info">
+								<div class="ingredients">${description}
+								</div>
+							</div>
+							<div class="card-buttons">
+								<button class="button button-primary button-add-cart">
+									<span class="button-card-text">В корзину</span>
+									<span class="button-cart-svg"></span>
+								</button>
+								<strong class="card-price-bold">${price} ₽</strong>
+							</div>
+            </div>`
+
+    $cardsMenu.append($card)
+  })
+}
+
+if (localStorage.getItem("restaurant")) {
+  const restaurant = JSON.parse(localStorage.getItem("restaurant"))
+
+  handleTitle(restaurant)
+
+  fetch(`./db/${restaurant.products}`)
+    .then(response => response.json())
+    .then(data => renderItems(data))
+    .catch(error => console.log("###e:", error))
+} else {
+  window.location.href = "/"
+}
--- js/partners.js
+++ js/partners.js
@@ -1,5 +1,56 @@
+const $cardsRestaurants = document.querySelector(".cards-restaurants")
+
 const renderItems = data => {
-  data.forEach(el => console.log(el))
+  data.forEach(item => {
+    const {
+      image,
+      name,
+      time_of_delivery,
+      stars,
+      price,
+      kitchen,
+      products
+    } = item
+    const $a = document.createElement("a")
+
+    $a.setAttribute("href", "/restaurant.html")
+    $a.classList.add("card")
+    $a.classList.add("card-restaurant")
+    $a.dataset.products = products
+
+    $a.innerHTML = `<img
+                src=${image}
+                alt=${name}
+                class="card-image"
+              />
+              <div class="card-text">
+                <div class="card-heading">
+                  <h3 class="card-title">${name}</h3>
+                  <span class="card-tag tag">${time_of_delivery} мин</span>
+                </div>
+                <div class="card-info">
+                  <div class="rating">
+                    ${stars}
+                  </div>
+                  <div class="price">От ${price} ₽</div>
+                  <div class="category">${kitchen}</div>
+                </div>
+              </div>`
+
+    $a.addEventListener("click", e => {
+      e.preventDefault()
+
+      if (localStorage.getItem("user")) {
+        localStorage.setItem("restaurant", JSON.stringify(item))
+        window.location.href = "/restaurant.html"
+      } else {
+        $modalAuth.style.display = "flex"
+        $inputLogin.focus()
+      }
+    })
+
+    $cardsRestaurants.append($a)
+  })
 }
 
 fetch("https://simple-food-market-default-rtdb.firebaseio.com/db/partners.json")
